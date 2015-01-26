2651f767cbf9437d50d1c33712ad02346863c7d9
koscoder <pogrebnoj.konstantin@gmail.com>
2015-01-26 17:03:50 +0200

    Modified role

--- roles/magento-standalone.rb
+++ roles/magento-standalone.rb
@@ -1,6 +1,6 @@
 name "magento-standalone"
 description "Magento standalone server"
-run_list "magento_standalone[default]"
+run_list "recipe[magento_standalone::default]"
 override_attributes({
   
 })
