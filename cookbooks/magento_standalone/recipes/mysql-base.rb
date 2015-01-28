43cdce8a7863b15c5e8370eae75f57c315c58e71
koscoder <pogrebnoj.konstantin@gmail.com>
2015-01-28 19:23:39 +0200

    php_appstack is defined as global var

--- cookbooks/magento_standalone/recipes/mysql-base.rb
+++ cookbooks/magento_standalone/recipes/mysql-base.rb
@@ -4,7 +4,7 @@
 #
 # Copyright (C) 2015
 
-php_appstack = node.default['php_appstack']
+$php_appstack = node.default['php_appstack']
 
 # Include the #secure_password method from the openssl cookbook
 Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
@@ -16,13 +16,13 @@ include_recipe 'encrypted_attributes'
 Chef::Recipe.send(:include, Chef::EncryptedAttributesHelpers)
 
 # We can use an attribute to enable or disable encryption (recommended for tests)
-# self.encrypted_attributes_enabled = node[php_appstack]['encrypt_attributes']
+# self.encrypted_attributes_enabled = node[$php_appstack]['encrypt_attributes']
 
 # Encrypted Attributes will be generated randomly and saved in in the
-# node[php_appstack]['mysql'] attribute encrypted.
+# node[$php_appstack]['mysql'] attribute encrypted.
 def generate_mysql_password(user)
   key = "server_#{user}_password"
-  encrypted_attribute_write([php_appstack, 'mysql', key]) { secure_password }
+  encrypted_attribute_write([$php_appstack, 'mysql', key]) { secure_password }
 end
 
 # Generate the encrypted passwords
